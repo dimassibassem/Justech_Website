ce23ac7c742ad6126d812254f0b745521bc8db9d
Bassem Dimassi <dimassibassem99@gmail.com>
2022-06-24 12:41:08 +0100

    starter-next with eslint and tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{html,js}',
+    './node_modules/tw-elements/dist/js/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+  ],
+}
