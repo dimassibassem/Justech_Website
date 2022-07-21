84c87fed4c7475f6d4a7c0df6fc876536b30dbd3
Bassem Dimassi <dimassibassem99@gmail.com>
2022-07-21 20:35:23 +0100

    Changing Title

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,11 @@
 import Head from 'next/head'
 
-import { CallToAction } from '@/components/CallToAction'
+
 import { Faqs } from '@/components/Faqs'
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { Hero } from '@/components/Hero'
-import { Pricing } from '@/components/Pricing'
+
 import { PrimaryFeatures } from '@/components/PrimaryFeatures'
 import { SecondaryFeatures } from '@/components/SecondaryFeatures'
 import { Testimonials } from '@/components/Testimonials'
