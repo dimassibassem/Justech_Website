bf5a7273c01de1831ebf280d4a54da8d74e256cc
Bassem Dimassi <dimassibassem99@gmail.com>
2022-07-21 12:52:45 +0100

    Adding map to ContactUs

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -7,7 +7,7 @@ export function ContactUs() {
   return (
     <section
       id='get-started-today'
-      className='relative overflow-hidden bg-blue-600 py-32'
+      className='relative overflow-hidden bg-blue-600 py-20'
     >
       <Image
         className='absolute top-1/2 left-1/2 max-w-none -translate-x-1/2 -translate-y-1/2'
@@ -22,7 +22,7 @@ export function ContactUs() {
           <div className='absolute inset-y-0 left-0 w-1/2' />
         </div>
         <div className='relative max-w-7xl mx-auto lg:grid lg:grid-cols-5'>
-          <div className='bg-gray-50 py-16 px-4 sm:px-6 lg:col-span-2 lg:px-8 lg:py-24 xl:pr-12'>
+          <div className='bg-gray-50 py-12 px-4 sm:px-6 lg:col-span-2 lg:px-8 lg:py-16 xl:pr-12'>
             <div className='max-w-lg mx-auto'>
               <h2 className='text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl'>Get in touch</h2>
               <p className='mt-3 text-lg leading-6 text-gray-500'>
@@ -62,71 +62,74 @@ export function ContactUs() {
               </p>
             </div>
           </div>
-          <div className='bg-white py-16 px-4 sm:px-6 lg:col-span-3 lg:py-24 lg:px-8 xl:pl-12'>
-            <div className='max-w-lg mx-auto lg:max-w-none'>
-              <form action='#' method='POST' className='grid grid-cols-1 gap-y-6'>
-                <div>
-                  <label htmlFor='full-name' className='sr-only'>
-                    Full name
-                  </label>
-                  <input
-                    type='text'
-                    name='full-name'
-                    id='full-name'
-                    autoComplete='name'
-                    className='block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md'
-                    placeholder='Full name'
-                  />
-                </div>
-                <div>
-                  <label htmlFor='email' className='sr-only'>
-                    Email
-                  </label>
-                  <input
-                    id='email'
-                    name='email'
-                    type='email'
-                    autoComplete='email'
-                    className='block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md'
-                    placeholder='Email'
-                  />
-                </div>
-                <div>
-                  <label htmlFor='phone' className='sr-only'>
-                    Phone
-                  </label>
-                  <input
-                    type='text'
-                    name='phone'
-                    id='phone'
-                    autoComplete='tel'
-                    className='block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md'
-                    placeholder='Phone'
-                  />
-                </div>
-                <div>
-                  <label htmlFor='message' className='sr-only'>
-                    Message
-                  </label>
-                  <textarea
-                    id='message'
-                    name='message'
-                    rows={4}
-                    className='block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border border-gray-300 rounded-md'
-                    placeholder='Message'
-                    defaultValue={''}
-                  />
-                </div>
-                <div>
-                  <button
-                    type='submit'
-                    className='inline-flex justify-center py-3 px-6 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-                  >
-                    Submit
-                  </button>
-                </div>
-              </form>
-            </div>
+          <div className='bg-white py-6 px-2 sm:px-3 lg:col-span-3 lg:py-8 lg:px-4 xl:pl-6'>
+            {/*<div className='max-w-lg mx-auto lg:max-w-none'>*/}
+            {/*  <form action='#' method='POST' className='grid grid-cols-1 gap-y-6'>*/}
+            {/*    <div>*/}
+            {/*      <label htmlFor='full-name' className='sr-only'>*/}
+            {/*        Full name*/}
+            {/*      </label>*/}
+            {/*      <input*/}
+            {/*        type='text'*/}
+            {/*        name='full-name'*/}
+            {/*        id='full-name'*/}
+            {/*        autoComplete='name'*/}
+            {/*        className='block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md'*/}
+            {/*        placeholder='Full name'*/}
+            {/*      />*/}
+            {/*    </div>*/}
+            {/*    <div>*/}
+            {/*      <label htmlFor='email' className='sr-only'>*/}
+            {/*        Email*/}
+            {/*      </label>*/}
+            {/*      <input*/}
+            {/*        id='email'*/}
+            {/*        name='email'*/}
+            {/*        type='email'*/}
+            {/*        autoComplete='email'*/}
+            {/*        className='block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md'*/}
+            {/*        placeholder='Email'*/}
+            {/*      />*/}
+            {/*    </div>*/}
+            {/*    <div>*/}
+            {/*      <label htmlFor='phone' className='sr-only'>*/}
+            {/*        Phone*/}
+            {/*      </label>*/}
+            {/*      <input*/}
+            {/*        type='text'*/}
+            {/*        name='phone'*/}
+            {/*        id='phone'*/}
+            {/*        autoComplete='tel'*/}
+            {/*        className='block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md'*/}
+            {/*        placeholder='Phone'*/}
+            {/*      />*/}
+            {/*    </div>*/}
+            {/*    <div>*/}
+            {/*      <label htmlFor='message' className='sr-only'>*/}
+            {/*        Message*/}
+            {/*      </label>*/}
+            {/*      <textarea*/}
+            {/*        id='message'*/}
+            {/*        name='message'*/}
+            {/*        rows={4}*/}
+            {/*        className='block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border border-gray-300 rounded-md'*/}
+            {/*        placeholder='Message'*/}
+            {/*        defaultValue={''}*/}
+            {/*      />*/}
+            {/*    </div>*/}
+            {/*    <div>*/}
+            {/*      <button*/}
+            {/*        type='submit'*/}
+            {/*        className='inline-flex justify-center py-3 px-6 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'*/}
+            {/*      >*/}
+            {/*        Submit*/}
+            {/*      </button>*/}
+            {/*    </div>*/}
+            {/*  </form>*/}
+            {/*</div>*/}
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9030.09529836839!2d10.196873752926768!3d36.85155647963583!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x21efe47d471f6bb1!2sJustech!5e0!3m2!1sfr!2stn!4v1465373613655&amp;z=20"
+              width="100%" height="400" frameBorder="0" allowFullScreen=""></iframe>
           </div>
         </div>
       </Container>
