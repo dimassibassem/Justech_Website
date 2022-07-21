a189279f17d6030298ed39bf6cd668f42055ec1a
Bassem Dimassi <dimassibassem99@gmail.com>
2022-07-21 12:45:03 +0100

    Responsive swiper

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -1,35 +1,37 @@
 import React from 'react'
 import clsx from 'clsx'
+import Image from 'next/future/image'
 
 
 function Feature({ feature, isActive, className, ...props }) {
   return (
     <div
-      className={clsx(className, !isActive && 'opacity-75 hover:opacity-100')}
+      className={clsx(className, !isActive && 'opacity-75 hover:opacity-100') + ' grid grid-cols-1 gap-4 p-4 '}
       {...props}
     >
       <div
-        className={clsx(
-          'w-9 rounded-lg',
-          isActive ? 'bg-blue-600' : 'bg-slate-500'
-        )}
+        // className={clsx(
+        //   'w-9 rounded-lg',
+        //   isActive ? 'bg-blue-600' : 'bg-slate-500'
+        // )}
       >
-        <svg aria-hidden='true' className='h-9 w-9' fill='none'>
-          <feature.icon />
-        </svg>
+        {/*<svg aria-hidden='true' className='h-9 w-9' fill='none'>*/}
+          {/*<feature.icon />*/}
+          <Image src={feature.image} alt='' width={250} height={200} />
+        {/*</svg>*/}
       </div>
-      <h3
-        className={clsx(
-          'mt-6 text-sm font-medium',
-          isActive ? 'text-blue-600' : 'text-slate-600'
-        )}
-      >
-        {feature.name}
-      </h3>
-      <p className='mt-2 font-display text-xl text-slate-900'>
+      {/*<h3*/}
+      {/*  className={clsx(*/}
+      {/*    'mt-6 text-sm font-medium',*/}
+      {/*    isActive ? 'text-blue-600' : 'text-slate-600'*/}
+      {/*  )}*/}
+      {/*>*/}
+      {/*  {feature.name}*/}
+      {/*</h3>*/}
+      <p className='mt-2 font-display text-md text-slate-800'>
         {feature.summary}
       </p>
-      <p className='mt-4 text-sm text-slate-600'>{feature.description}</p>
+      {/*<p className='mt-4 text-sm text-slate-600'>{feature.description}</p>*/}
     </div>
   )
 
--- src/components/SwiperGallery.js
+++ src/components/SwiperGallery.js
@@ -2,34 +2,41 @@ import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Tab } from '@headlessui/react'
 import 'swiper/css'
+import 'swiper/css/autoplay'
+
 import Feature from '@/components/Feature'
 
-export function SwiperGallery({features,selectedIndex}) {
+import { Autoplay } from 'swiper'
+
+export function SwiperGallery({ features, selectedIndex }) {
+
   return (
     <Swiper
       spaceBetween={50}
-      slidesPerView={3}
+      slidesPerView={5}
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
+      modules={[Autoplay]}
+      autoplay
     >
 
       {features?.map((feature, featureIndex) => (
         <SwiperSlide key={featureIndex}>
-        <Feature
-          key={feature.name}
-          feature={{
-            ...feature,
-            name: (
-              <Tab className="[&:not(:focus-visible)]:focus:outline-none">
-                <span className="absolute inset-0" />
-                {feature.name}
-              </Tab>
-            ),
-          }}
-          isActive={featureIndex === selectedIndex}
-          className="relative"
-        />
-         </SwiperSlide>
+          <Feature
+            key={feature.name}
+            feature={{
+              ...feature,
+              name: (
+                <Tab className='[&:not(:focus-visible)]:focus:outline-none'>
+                  <span className='absolute inset-0' />
+                  {feature.name}
+                </Tab>
+              )
+            }}
+            isActive={featureIndex === selectedIndex}
+            className='relative'
+          />
+        </SwiperSlide>
       ))}
 
     </Swiper>
