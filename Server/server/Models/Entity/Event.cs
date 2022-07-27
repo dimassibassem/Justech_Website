e1563e451e6bc3e868953515dc487b4a5bd909de
Bassem Dimassi <dimassibassem99@gmail.com>
2022-07-27 09:39:55 +0100

    merge

--- Server/server/Models/Entity/Event.cs
+++ Server/server/Models/Entity/Event.cs
@@ -2,23 +2,27 @@
 
 public class Event
 {
-
     public long Id { get; set; }
     public string? EventName { get; set; }
     public string? Thumbnail { get; set; }
- 
+    public string? Location { get; set; }
+
     public string? Description { get; set; }
+
+    public string? Date { get; set; }
 
 
     public Event()
     {
     }
 
-    public Event(long id , string? eventName, string thumbnail, string description)
+    public Event(long id, string? eventName, string thumbnail, string description, string location, string date)
     {
         Id = id;
         EventName = eventName;
-        Description = description;
         Thumbnail = thumbnail;
+        Description = description;
+        Location = location;
+        Date = date;
     }
 }
