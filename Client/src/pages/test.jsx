bfbfbe43090f7ba09dc6085093feeb6afb28ff4b
Bassem Dimassi <dimassibassem99@gmail.com>
2022-08-11 01:33:21 +0100

    :construction: Starting Chat construction

--- Client/src/pages/test.jsx
+++ Client/src/pages/test.jsx
@@ -1,29 +1,13 @@
 import React from 'react'
-import ImageGallery from 'react-image-gallery';
-import "node_modules/react-image-gallery/styles/css/image-gallery.css";
+import Chat from "@/components/chat/Chat";
 
 
 function Test() {
+    return (
 
-    const images = [
-        {
-            original: 'https://picsum.photos/id/1018/1000/600/',
-            thumbnail: 'https://picsum.photos/id/1018/250/150/',
-        },
-        {
-            original: 'https://picsum.photos/id/1015/1000/600/',
-            thumbnail: 'https://picsum.photos/id/1015/250/150/',
-        },
-        {
-            original: 'https://picsum.photos/id/1019/1000/600/',
-            thumbnail: 'https://picsum.photos/id/1019/250/150/',
-        },
-    ];
-  return (
-    <div>
-      <ImageGallery items={images}/>
-    </div>
-  )
+        <Chat/>
+
+    )
 }
 
 export default Test
