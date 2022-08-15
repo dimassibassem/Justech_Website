1fbc941f7c8262e304919f0ed650695fb259d688
Bassem Dimassi <dimassibassem99@gmail.com>
2022-08-15 14:11:12 +0100

    :closed_lock_with_key: Securing DeletePartnerBy

--- Server/server/Controllers/PartnersController.cs
+++ Server/server/Controllers/PartnersController.cs
@@ -78,6 +78,22 @@ namespace server.Controllers
         [HttpDelete("DeletePartnerBy")]
         public JsonResult DeletePartnerBy(string field, string value)
         {
+            if (!Request.Headers.ContainsKey("Authorization"))
+                return new JsonResult(new JsonResponse
+                {
+                    Success = false,
+                    Message = "unAuthorized"
+                });
+
+            var token = Request.Headers["Authorization"];
+            token = token.ToString().Substring(7);
+            if (!BllAuth.IsTokenValid(token))
+                return new JsonResult(new JsonResponse
+                {
+                    Success = false,
+                    Message = "unAuthorized"
+                });
+
             if (string.IsNullOrEmpty(field) || string.IsNullOrEmpty(value))
             {
                 JsonResponse jsonResponse = new JsonResponse
