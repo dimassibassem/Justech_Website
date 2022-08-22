9897eed91ef90a0bfdccd9a43837030f98fc8761
Bassem Dimassi <dimassibassem99@gmail.com>
2022-08-23 00:34:12 +0100

    Refactoring Code

--- Client/src/pages/index.jsx
+++ Client/src/pages/index.jsx
@@ -24,25 +24,25 @@ export default function Home() {
     }, [])
 
     return (
-        <div className='relative bg-gray-200'>
-            <Head>
-                <title>Justech</title>
-                <meta
-                    name='description'
-                    content='Justech is a Tunisian company specializing in the field of technological innovation and in particular digital content, e-Learning, virtual reference libraries and e-business solutions. Justech is distinguished not only by solid partners and high-level services but also by a multilingual, dynamic and qualified team.'
-                />
-            </Head>
-            <Header/>
-            <main>
-                <Hero/>
-                <StickyButton/>
-                <PrimaryFeatures/>
-                <Partners/>
-                <Testimonials/>
-                <Events/>
-            </main>
-            <ContactUs/>
-            <Footer/>
-        </div>
+            <div className='relative bg-gray-200'>
+                <Head>
+                    <title>Justech</title>
+                    <meta
+                        name='description'
+                        content='Justech is a Tunisian company specializing in the field of technological innovation and in particular digital content, e-Learning, virtual reference libraries and e-business solutions. Justech is distinguished not only by solid partners and high-level services but also by a multilingual, dynamic and qualified team.'
+                    />
+                </Head>
+                <Header/>
+                <main>
+                    <Hero/>
+                    <StickyButton/>
+                    <PrimaryFeatures/>
+                    <Partners/>
+                    <Testimonials/>
+                    <Events/>
+                </main>
+                <ContactUs/>
+                <Footer/>
+            </div>
     )
 }
